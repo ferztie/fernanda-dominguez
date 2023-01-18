2a90f6ed8f9dc3026793ee32cbb35606908830a8
ferztie <vozzzoa@gmail.com>
2023-01-18 14:16:23 -0600

    secciones de productos en una etapa1

--- main.js
+++ main.js
@@ -4,16 +4,13 @@ import { scroll, animate, ScrollOffset } from "https://cdn.skypack.dev/motion@10
 const $sectionPin = document.querySelector("#sectionPin");
 const $slidingContent = document.querySelector(".pin-wrap");
 
-const sectionHeightInVh = 500; // 👈 The scrolling distance over which the horizontal section should slide across
+const sectionHeightInVh = 500; 
 
 // Adjust wrapper
-// - Change height so that we have room to scroll in
-// - Add fix to make position: sticky work
 $sectionPin.style.height = `${sectionHeightInVh}vh`;
 $sectionPin.style.overflow = `visible`;
 
 // Adjust content
-// - Make it stick to the top
 $slidingContent.style.position = "sticky";
 $slidingContent.style.top = 0;
 
@@ -23,4 +20,28 @@ scroll(
 		target: $sectionPin,
 		offset: ScrollOffset.enter,
 	}
-);
+)
+
+let tabContent = document.querySelectorAll(".container__inner");
+let tabItem = document.querySelectorAll(".container__item");
+
+// For each element with class 'container__item'
+for (let i = 0; i < tabItem.length; i++) {
+  // if the element was hovered
+  //you can replace mouseover with click
+  tabItem[i].addEventListener("mouseover", () => {
+    // Add to all containers class 'container__inner_hidden'
+    tabContent.forEach((item) => {
+      item.classList.add("container__inner_hidden");
+    });
+    // Clean all links from class 'container__item_active'
+    tabItem.forEach((item) => {
+      item.classList.remove("container__item_active");
+    });
+    // Make visible correct tab content and add class to item
+    tabContent[i].classList.remove("container__inner_hidden");
+    tabItem[i].classList.add("container__item_active");
+  });
+}
+
+
