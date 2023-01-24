a0fc42f98e322b92461400553271da419b652b44
ferztie <vozzzoa@gmail.com>
2023-01-23 19:21:40 -0600

    dark-mode y pdf lentes purpura en img

--- main.js
+++ main.js
@@ -44,243 +44,226 @@ for (let i = 0; i < tabItem.length; i++) {
   });
 }
 
-"use strict"; // Paul Slaymaker, paul25882@gmail.com
-const body=document.getElementsByTagName("body").item(0);
-body.style.background="#000";
-const TP=2*Math.PI;
-const CSIZE=360;
 
-const ctx=(()=>{
-  let d=document.createElement("div");
-  d.style.textAlign="center";
-  body.append(d);
-  let c=document.createElement("canvas");
-  c.width=c.height=2*CSIZE;
-  d.append(c);
-  return c.getContext("2d");
-})();
-ctx.setTransform(1,0,0,1,CSIZE,CSIZE);
-ctx.lineCap="round";
-
-onresize=()=>{ 
-  let D=Math.min(window.innerWidth,window.innerHeight)-40; 
-  ctx.canvas.style.width=ctx.canvas.style.height=D+"px";
+function loadTheme(theme) {
+	const id = Math.floor(1000 + Math.random() * 9000); //To generate a random 4 digit number
+	const link = document.querySelector("link");
+	link.href = `${theme}.css?id=${id}`; //Add the ID so the CSS file is unique each load. This prevents browser caching.
 }
 
-const getRandomInt=(min,max,low)=>{
-  if (low) return Math.floor(Math.random()*Math.random()*(max-min))+min;
-  else return Math.floor(Math.random()*(max-min))+min;
+// Same eventListener but calling loadTheme() with applicable parameter
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+	const modeChange = event.matches ? "dark" : "light";
+	const autoMode = document.querySelector(".selected");
+	if (autoMode.classList.contains("auto")) {
+		if (modeChange === "dark") {
+			loadTheme("dark");
+		} else {
+			loadTheme("light");
+		}
+	}
+});
+
+import gsap from 'https://cdn.skypack.dev/gsap@3.11.0'
+
+console.clear()
+
+const CANVAS = document.querySelector('#rain')
+
+const DEFAULT_OPTIONS = {
+  size: () => window.innerWidth * 0.015,
+  family: 'JetBrains Mono, monospace',
+  fps: 24,
+  hue: 120,
+  limiter: 0.25,
+  glyphs:
+    'ラドクリフマラソンわたしワタシんょンョたばこタバコとうきょうトウキョウ0123456789±!@#$%^&*()_+ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  // glyphs: '01'
+
 }
 
-var colors=[];
-var hues=[];
-var setColors=()=>{
-  colors=[];
-  let colorCount=3;
-  let h=getRandomInt(180,270);
-  for (let i=0; i<colorCount; i++) {
-    let hd=Math.round(180/colorCount)*i+getRandomInt(-10,10);
-    let hue=(h+hd)%360;
-    hues.splice(getRandomInt(0,hues.length+1),0,hue);
-  }
-  for (let i=0; i<colorCount; i++) colors[i]="hsl("+hues[i]+",98%,40%)";
+const DigitalRain = function (el, options) {
+  if (el.tagName !== 'CANVAS') return console.error('Need a canvas element')
+  const self = this
+  self.__ratio = window.devicePixelRatio || 1
+  self.canvas = el
+  self.options = options
+  self.size = options.size
+  self.glyphs = self.options.glyphs.split('')
+  self.context = el.getContext('2d')
+  self.setSize()
+  self.setTracker()
+  self.init()
+  return self
 }
 
-const df=[
-  (point)=>{ return pts[point.i+1][point.j]; },
-  (point)=>{ return pts[point.i][point.j+1]; },
-  (point)=>{ return pts[point.i-1][point.j]; },
-  (point)=>{ return pts[point.i][point.j-1]; }
-];
+// Sending back a column Object that represents the state of a column
+DigitalRain.prototype.setColumn = function (column = {}) {
+  const self = this
+  const { glyphs } = self
+  // Set a destination and record len && lastLen
+  const len = gsap.utils.random(6, self.rows, 1)
+  const lastLen = column.len || len
+  // const destination = self.rows + len
+  const destination = gsap.utils.random(self.rows * 0.1, self.rows + len, 1)
+  const lastDestination = column.destination || destination
+  // Tracking the last Destination needs a tail off to roll out the old stream
+  const tailEnd = lastDestination + lastLen
 
-const iptX=[[2,3,1,0],[3,2,0,1]];
+  // If you have column.chars reuse else reset up to destination
+  let chars = column.chars || []
 
-var getFalsePoint=()=>{
-  let pt=pts[getRandomInt(1,CT-1,true)][getRandomInt(1,CT-1,true)];
-  while (pt.d) {
-    pt=pts[getRandomInt(1,CT-1,true)][getRandomInt(1,CT-1,true)];
-  } 
-  return pt;
-}
+  // When you come in, cache the last set of chars
+  let cacheChars = [...chars]
 
-function SPath(idx) {
-  let initPoint=getFalsePoint();
-  initPoint.d=true;
-  initPoint.ppt=pts[0][0];
-  this.pa=[initPoint];
-  this.path;
-  this.term;
-  this.ipt=iptX[0];
-  this.grow=()=>{
-    for (let ci=this.pa.length-1; ci>=0; ci--) {
-      let pt=this.pa[ci];
-      if (pt==this.growPoint) continue;
-      for (let ic=0; ic<4; ic++) {
-        let dfunc=df[this.ipt[ic]];
-	let cpt=dfunc(pt);
-	if (cpt.d) continue;
-    if (cpt==this.shrinkPoint) continue;
-	cpt.d=true;
-	pt.cpa.push(cpt);
-	cpt.ppt=pt;
-	this.pa.push(cpt);
-	if (this.growPoint) this.growPoint2=cpt;
-	else this.growPoint=cpt;
-	return true;
-      }
-    }
-    return false;
-  }
-  this.setShrink=()=>{
-    this.shrinkPoint=false;
-    if (this.pa.length<3) return;
-    let pt=this.pa[0];
-    if (pt.cpa.length<2) {
-      this.shrinkPoint=pt;
-      return;
-    }
-    for (let i=1; i<this.pa.length-1; i++) { // always shrink from oldest end, if possible
-      let pt=this.pa[i];
-      if (pt==this.growPoint) continue;
-      if (pt.cpa.length==0) {
-        this.shrinkPoint=pt;
-        return;
-      }
-    }
-this.shrinkPoint=false;
-  }
-  this.shrink2=()=>{
-    if (!this.shrinkPoint) return;
-    let pt=this.shrinkPoint;
-    if (this.pa[0]==pt) {
-      if (!pt.ppt.d) pt.ppt=false;
+  chars = new Array(Math.max(destination, chars.length)).fill().map((entry, index) => {
+    if (index <= destination) {
+      return self.glyphs[gsap.utils.random(0, self.glyphs.length - 1, 1)]
     } else {
-      if (pt.ppt.cpa.length) {
-      let idx=pt.ppt.cpa.indexOf(pt);
-        pt.ppt.cpa.splice(idx,1);
-      } 
+      return cacheChars[index]
     }
-    pt.cpa=[];
-    let idx2=this.pa.indexOf(pt);
-    this.pa.splice(idx2,1);
-    pt.d=false;
+  })
+
+  const row = gsap.utils.random(-self.rows, -1, 1)
+
+  // column.hue = column.hue || gsap.utils.random(0, 359, 1)
+
+  return {
+    ...column,
+    chars,
+    cacheChars,
+    destination,
+    lastDestination,
+    lastLen,
+    tailEnd,
+    tailCounter: lastDestination,
+    row,
+    len,
   }
-  this.setPath=()=>{
-    this.path=new Path2D(); 
-    if (this.pa.length==1) {
-      this.path.moveTo(this.pa[0].x,this.pa[0].y);
-      this.path.lineTo(this.pa[0].x,this.pa[0].y);
-      return;
+}
+
+DigitalRain.prototype.setTracker = function () {
+  const self = this
+  self.tracker = new Array(self.columns).fill().map(() => self.setColumn())
+}
+
+DigitalRain.prototype.reset = function () {
+  const self = this
+  self.context.clearRect(0, 0, self.canvas.width, self.canvas.height)
+  self.setSize()
+  self.setTracker()
+}
+
+DigitalRain.prototype.init = function () {
+  const self = this
+  self.renderMatrix = () => self.render()
+  self.resetOnSize = () => self.reset()
+  window.addEventListener('resize', self.resetOnSize)
+  gsap.ticker.add(self.renderMatrix)
+  gsap.ticker.fps(self.options.fps)
+  self.pause = () => {
+    gsap.ticker.remove(self.renderMatrix)
+  }
+  self.play = () => {
+    gsap.ticker.add(self.renderMatrix)
+  }
+}
+
+DigitalRain.prototype.getColor = function (
+  x,
+  y,
+  {
+    hue,
+    row,
+    len,
+    lastLen,
+    lastDestination,
+    tailCounter,
+  }
+) {
+  const self = this
+  // If y > row but less than last destination, work out the color as if row === column.lastDestination
+  const lower = 0.1
+  const upper = 1
+  let alpha = 0.1
+
+  if (y <= row) {
+    alpha = gsap.utils.clamp(
+      lower,
+      upper,
+      gsap.utils.mapRange(-len, 0, lower, upper)(y - row)
+    )
+  } else if (y > row && y <= lastDestination) {
+    alpha = gsap.utils.clamp(
+      lower,
+      upper,
+      gsap.utils.mapRange(-lastLen, 0, lower, upper)(y - tailCounter)
+    )
+  } else if (y > lastDestination) {
+    alpha = lower
+  }
+  return `hsl(${hue || self.options.hue}, 100%, ${row === y ? 100 : 70}%, ${alpha})`
+}
+
+DigitalRain.prototype.render = function () {
+  const self = this
+  self.context.clearRect(0, 0, self.canvas.width, self.canvas.height)
+  // Need to try and iterate over every cell in the Matrix...
+  for (let c = 0; c < self.characters; c++) {
+    const x = c % self.columns
+    const y = Math.floor(c / self.columns)
+    const column = self.tracker[x]
+
+    // On the first row, let's bump the index
+    if (y === 0 && Math.random() > 0.1) {
+      column.row += 1
     }
-    for (let i=1; i<this.pa.length; i++) {
-      let pt=this.pa[i];
-      if (pt.ppt==this.shrinkPoint) {
-	let pt2=this.pa[0];
-	this.path.moveTo((1-frac)*pt.ppt.x+frac*pt2.cpa[0].x,(1-frac)*pt.ppt.y+frac*pt2.cpa[0].y);
-	this.path.lineTo(pt2.cpa[0].x,pt2.cpa[0].y);
-      } else {
-	if (pt==this.shrinkPoint) {
-	  this.path.moveTo((1-frac)*pt.x+frac*pt.ppt.x,(1-frac)*pt.y+frac*pt.ppt.y);
-	} else if (pt==this.growPoint || pt==this.growPoint2) {
-	  this.path.moveTo((1-frac)*pt.ppt.x+frac*pt.x,(1-frac)*pt.ppt.y+frac*pt.y);
-	} else this.path.moveTo(pt.x,pt.y);
-	this.path.lineTo(pt.ppt.x,pt.ppt.y);
+
+    if (column.tailCounter !== column.tailOff && y === 0) {
+      column.tailCounter += 1
+    }
+
+    const row = column.row
+    const chars = column[y > row ? 'cacheChars' : 'chars']
+    
+    self.context.fillStyle = self.getColor(x, y, column)
+
+
+    if (chars[y]) {
+      if (Math.random() > 0.999 && y > row) {
+        column.cacheChars[y] = column.chars[y] = ''
       }
-    }
-  }
-  this.morph=()=>{ this.ipt=iptX[getRandomInt(0,2)]; }
-}
-
-function start() {
-  if (stopped) {
-    requestAnimationFrame(animate);
-    stopped=false;
-  } else {
-    stopped=true;
-  }
-}
-ctx.canvas.addEventListener("click", start, false);
-
-var stopped=true;
-var t=1;
-var n=0;
-var frac=0;
-var dur=16;
-function animate(ts) {
-  if (stopped) return;
-  t++;
-  if (t==dur) {
-    for (let i=0; i<hues.length; i++) {
-      hues[i]=++hues[i]%360;
-      colors[i]="hsl("+hues[i]+",98%,40%)";
-    }
-    for (let i=0; i<sp.length; i++) {
-      sp[i].shrink2();
-      sp[i].growPoint=false;
-      sp[i].growPoint2=false;
-      if (sp[i].pa.length<GC) {
-	if (sp[i].grow()) {
-	  if (sp[i].pa.length<GC) sp[i].grow();
-	} 
+      if (Math.random() > 0.99 && (y < row && y < column.destination && y > (column.destination - column.len))) {
+        column.cacheChars[y] = column.chars[y] = self.glyphs[gsap.utils.random(0, self.glyphs.length - 1, 1)]
       }
-      sp[i].setShrink();
+      self.context.fillText(
+        chars[y],
+        (x + 0.5) * self.fontSize,
+        (y + 1) * self.fontSize
+      )
     }
-    if (++n>32) {
-      GC=[8,9,7,10,6,11,5,12,4,13,3,14,15][getRandomInt(0,12,true)];
-      for (let i=0; i<sp.length; i++) sp[i].morph();
-      n=0;
+    // Reset the column if we go past destination
+    if (row > column.destination) {
+      self.tracker[x] = self.setColumn(column)
     }
-    t=0;
-  }
-  frac=t/dur;
-  draw();
-  requestAnimationFrame(animate);
-}
-
-const CT=14;
-var GC=10;//getRandomInt(7,11);
-
-const dm1=new DOMMatrix([-1,0,0,1,0,0]);
-const dm2=new DOMMatrix([1,0,0,-1,0,0]);
-
-var draw=()=>{
-  ctx.clearRect(-CSIZE,-CSIZE,2*CSIZE,2*CSIZE);
-  for (let i=0; i<sp.length; i++) {
-    sp[i].setPath();
-    let p=new Path2D(sp[i].path);
-    p.addPath(p,dm1);
-    p.addPath(p,dm2);
-    ctx.globalAlpha=1;
-    ctx.lineWidth=Math.round(270/CT);
-    ctx.strokeStyle=colors[i%colors.length];
-    ctx.stroke(p);
-    ctx.globalAlpha=0.4;
-    ctx.lineWidth=Math.round(90/CT);
-    ctx.strokeStyle="white";
-    ctx.stroke(p);
   }
 }
 
-const pts=new Array();
-const inc=CSIZE/(CT-2);
-
-for (let i=0; i<CT; i++) {
-  pts.push(new Array());
-  for (let j=0; j<CT; j++) {
-    pts[i].push(new Array());
-    pts[i][j]={"x":-inc+i*inc,"y":-inc+j*inc,"i":i,"j":j,"cpa":[]};
-    if (i==0 || i==CT-1 || j==0 || j==CT-1) pts[i][j].d=true;
-  }
+DigitalRain.prototype.setSize = function () {
+  const self = this
+  const { height, width } = self.canvas.getBoundingClientRect()
+  self.canvas.height = height * self.__ratio
+  self.canvas.width = width * self.__ratio
+  // Set the font size and get the rows/columns
+  self.fontSize = Math.ceil(typeof self.size === 'function' ? self.size() : self.size)
+  self.columns = Math.ceil(self.canvas.width / self.fontSize)
+  // self.columns = 1
+  self.rows = Math.ceil(self.canvas.height / self.fontSize)
+  self.characters = self.rows * self.columns
+  self.context.font = `${self.fontSize}px ${self.options.family}`
+  self.context.textAlign = 'center'
 }
 
-var sp=new Array(9);
+window.myDigitalRain = new DigitalRain(CANVAS, DEFAULT_OPTIONS)
 
-for (let i=0; i<sp.length; i++) {
-  sp[i]=new SPath(i);
-  if (sp[i].grow()) sp[i].grow();
-}
 
-onresize();
-setColors();
-
-start();
