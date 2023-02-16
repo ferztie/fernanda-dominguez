5a17b4fdc684ce9e840ebd317dca5abd01b6652d
ferztie <vozzzoa@gmail.com>
2023-02-15 18:43:48 -0600

    fixing stuff

--- main.js
+++ main.js
@@ -23,3 +23,36 @@ toggle.addEventListener('change', function(event) {
         localStorage.setItem('theme', 'light');
     }
 });
+
+//Language
+
+function translatePage(language) {
+    // Create an object to hold translations
+    var translations = {};
+    
+    // Populate the translations object based on the selected language
+    switch (language) {
+      case 'es':
+        translations.title = 'Título en español';
+        translations.subtitle = 'Subtítulo en español';
+        // add more translations as needed
+        break;
+      case 'fr':
+        translations.title = 'Titre en français';
+        translations.subtitle = 'Sous-titre en français';
+        // add more translations as needed
+        break;
+      default:
+        translations.title = 'Title in English';
+        translations.subtitle = 'Subtitle in English';
+        // add more translations as needed
+        break;
+    }
+    
+    // Use jQuery or vanilla JavaScript to replace the text of each element
+    // with its translated value from the translations object
+    $('h1').text(translations.title);
+    $('h2').text(translations.subtitle);
+    // add more elements as needed
+  }
+  
